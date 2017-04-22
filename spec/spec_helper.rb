cd4513cffb9fd0758516ac4942dc867a74366221
Robert Kovacs <robi.kovacs11@gmail.com>
2017-04-22 17:24:06 +0300

    Remove Auth0 secrets from test env + Add paperclip cleanup on test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,8 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.include Paperclip::Shoulda::Matchers
+
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
+  end
 end
