6e464cac6567d37f16370ab2a163ae9697101617
Robert Kovacs <robi.kovacs11@gmail.com>
2017-04-27 20:23:31 +0300

    Add description on serializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :created_at, :updated_at, :url
+  attributes :id, :name, :created_at, :updated_at, :url, :description
 
   def url
     object.try(:track).try(:url)
