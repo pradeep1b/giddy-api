72564bc79dd333f4f0f713219711f838549fcf7a
Robert Kovacs <robi.kovacs11@gmail.com>
2017-04-27 20:05:55 +0300

    Add description on activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422214152) do
+ActiveRecord::Schema.define(version: 20170427170512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170422214152) do
     t.string   "track_content_type"
     t.integer  "track_file_size"
     t.datetime "track_updated_at"
+    t.text     "description"
     t.index ["user_id"], name: "index_activities_on_user_id", using: :btree
   end
 
